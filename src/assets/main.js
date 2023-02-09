176c47e1f80b95f47fd6c46d6b78a15f907e101d
juan david perez garcia <juandavid0859@gmail.com>
2023-02-09 17:25:13 -0500

    landingpage

---
+++ src/assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC-40FKj2BWhPpltodPLc8RA&part=snippet%2Cid&order=date&maxResults=3';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6dd9a346f7mshcf0f8147784d1bdp1f4705jsn4dedf5ea1368',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData (urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async() => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+        `)}
+        
+        `;
+    }   catch {
+
+    }
+})();
